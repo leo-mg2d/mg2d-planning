b0780f6a709f640519a7538418e38f122f698178
leo-mg2d <kznhf8tgw8@privaterelay.appleid.com>
2026-08-03 23:03:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.MG2D_CONFIG = {
-  supabaseUrl: "https://kfugtjwrrsajkzdksazk.supabase.co/rest/v1/",
+  supabaseUrl: "https://kfugtjwrrsajkzdksazk.supabase.co",
   supabaseAnonKey: "sb_publishable_1S_Y2t90szywtAjM6JUS7g_tKd4RON2"
 };
